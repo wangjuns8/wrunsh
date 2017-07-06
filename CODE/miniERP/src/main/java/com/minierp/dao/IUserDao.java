949f85b5e3c2df5e2f804710381b898a09a0e40f
wj <wangjuns8@163.com>
2017-07-06 22:41:19 +0800

    init project

---
+++ CODE/miniERP/src/main/java/com/minierp/dao/IUserDao.java
@@ -0,0 +1,18 @@
+package com.minierp.dao;
+
+import com.minierp.model.User;
+
+
+public interface IUserDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ CODE/miniERP/src/main/java/com/minierp/service/IUserService.java
@@ -0,0 +1,7 @@
+package com.minierp.service;
+
+import com.minierp.model.User;
+
+public interface IUserService {
+	public User getUserById(int userId);  
+}
